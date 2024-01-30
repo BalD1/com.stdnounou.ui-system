687de4166ca1622bbaf884c7b2b015789f5ac72c
Florian <florianhusson@hotmail.fr>
2024-01-30 17:23:17 +0100

    update ver

--- Runtime/Scripts/StdNounou_UI_Info.cs
+++ Runtime/Scripts/StdNounou_UI_Info.cs
@@ -1,9 +1,9 @@
-#define STDNOUNOU_UI_1_0_2
+#define STDNOUNOU_UI_1_0_3
 
 namespace StdNounou.UI
 {
     public static class StdNounou_UI_Info
     {
-        public const string VERSION = "1.0.2";
+        public const string VERSION = "1.0.3";
     }
 }
